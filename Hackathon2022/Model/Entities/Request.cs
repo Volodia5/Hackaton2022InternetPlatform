708634a6ebc0af4f78bd6b72da3bf77b87903fbe
ZewsSD <76225337+ZewsSD@users.noreply.github.com>
2022-04-19 16:18:35 +0300

    Add files via upload

---
+++ Hackathon2022/Model/Entities/Request.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackatonInternetPlatform.Model
+{
+    [Serializable]
+    internal class Request
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public string ProductType { get; set; }
+        public int Cost { get; set; }
+        public char Currency { get; set; }
+        public string PayMethod { get; set; }
+        public string DeliveryAdress { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
