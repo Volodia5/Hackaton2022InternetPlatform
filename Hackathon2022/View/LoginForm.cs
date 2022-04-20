0ded32a77348d8513459316e761f6714f17c9ad3
ZewsSD <76225337+ZewsSD@users.noreply.github.com>
2022-04-20 22:59:59 +0300

    Add files via upload

---
+++ Hackathon2022/View/LoginForm.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Windows.Forms;
+
+namespace HackatonInternetPlatform
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonVisiblePassword_Click(object sender, EventArgs e)
+        {
+            if (textBoxPassword.PasswordChar == default)
+            {
+                textBoxPassword.PasswordChar = '*';
+                return;
+            }
+
+            if (textBoxPassword.PasswordChar == '*')
+            {
+                textBoxPassword.PasswordChar = default;
+            }
+        }
+    }
+}
